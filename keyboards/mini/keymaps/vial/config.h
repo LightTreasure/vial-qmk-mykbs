ee5778aa7a2034c9a356d0b754a93da78207ec73
Alok Meshram <meshram.alok@gmail.com>
2024-05-18 02:13:45 -0700

    Added inital Vial support

---
+++ keyboards/mini/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+// Copyright 2024 Alok Meshram
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x77, 0xDF, 0x15, 0xDC, 0x0B, 0x9C, 0xBB, 0xDF}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 3 }
---
+++ keyboards/mini/keymaps/vial/rules.mk
@@ -0,0 +1,17 @@
+# MCU name
+MCU = atmega32u4
+
+# Bootloader selection
+#   Teensy       halfkay
+#   Pro Micro    caterina
+#   Atmel DFU    atmel-dfu
+#   LUFA DFU     lufa-dfu
+#   QMK DFU      qmk-dfu
+#   ATmega32A    bootloadHID
+#   ATmega328P   USBasp
+BOOTLOADER = caterina
+
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+TAP_DANCE_ENABLE = no
+KEY_OVERRIDE_ENABLE = no
