d108f73a7c5c9f5911e1ac4c2a9ac65ebea80b6b
Alok Meshram <meshram.alok@gmail.com>
2024-05-17 20:01:47 -0700

    Moved most config to info.json, lint check passing

--- keyboards/mini/rules.mk
+++ keyboards/mini/rules.mk
@@ -9,16 +9,4 @@ MCU = atmega32u4
 #   QMK DFU      qmk-dfu
 #   ATmega32A    bootloadHID
 #   ATmega328P   USBasp
-BOOTLOADER = caterina       
-
-# Special features
-# EXTRAKEY_ENABLE   - Enable Audio control and System control keys
-# NKRO_ENABLE       - Enable N-Key Rollover
-# CONSOLE_ENABLE    - Enable Console for debug
-# COMMAND_ENABLE    - Commands for debug and configuration
-# MOUSEKEY_ENABLE   - Enable Mouse control keys
-EXTRAKEY_ENABLE 	= yes
-NKRO_ENABLE 		  = yes
-CONSOLE_ENABLE		= yes
-COMMAND_ENABLE		= yes
-MOUSEKEY_ENABLE 	= yes
+BOOTLOADER = caterina
